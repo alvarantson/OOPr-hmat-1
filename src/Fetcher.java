1d351ac782b2ec7acb586ed5faf086b224c20b5b
Alvar Antson laptop <alvar.antson@real.edu.ee>
2019-03-12 09:19:26 +0200

    ilus getPostimees + Fetcher klass

--- src/Fetcher.java
+++ src/Fetcher.java
@@ -6,36 +6,96 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Fetcher {
     private String pealkiri;
     private String link;
     private String aeg;
     private String artikkel;
+    private String portaal;
 
+    public Fetcher(String pealkiri, String link, String aeg, String artikkel, String portaal) {
+        this.pealkiri = pealkiri;
+        this.link = link;
+        this.aeg = aeg;
+        this.artikkel = artikkel;
+        this.portaal = portaal;
+    }
 
-    public static void getPostimees() throws IOException {
+    public String getPealkiri() {
+        return pealkiri;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getAeg() {
+        return aeg;
+    }
+
+    public String getArtikkel() {
+        return artikkel;
+    }
+
+    public String getPortaal() {
+        return portaal;
+    }
+
+    @Override
+    public String toString() {
+        return "Fetcher{" +
+                "pealkiri='" + pealkiri + '\'' +
+                ", link='" + link + '\'' +
+                ", aeg='" + aeg + '\'' +
+                ", artikkel='" + artikkel + '\'' +
+                ", portaal='" + portaal + '\'' +
+                '}';
+    }
+
+    public static ArrayList<Fetcher> getPostimees() throws IOException, InterruptedException {
         String url = "https://postimees.ee";
         org.jsoup.nodes.Document document = Jsoup.connect(url).get();
-        //ArrayList<Fetcher>;
+        ArrayList<Fetcher> tagastus = new ArrayList<Fetcher>();
+
+
+        String pealkiri;
+        String link;
+        String aeg;
+        String artikliSisu;
 
         Elements text = document.select(".list-article__url");
         for (org.jsoup.nodes.Element item : text) {
-            String pealkiri = item.select(".list-article__headline").text();
-            System.out.println(pealkiri);
-            String link = item.attr("href");
-            System.out.println(link);
-            System.out.println("artikli sisu:");
+            try {
+                pealkiri = item.select(".list-article__headline").text();
+                //System.out.println(pealkiri);
+                link = item.attr("href");
+                //System.out.println(link);
+                //System.out.println("artikli sisu:");
 
-            Document artikkel = Jsoup.connect(item.attr("href")).get();
-            String aeg = artikkel.select(".article__publish-date").first().text();
-            System.out.println(aeg);
-            String artikliSisu = "";
-            for (Element tekst: artikkel.select(".article-body__item").select("p")){
-                artikliSisu = artikliSisu + tekst.text() + "\n";
+
+                Document artikkel = Jsoup.connect(item.attr("href")).get();
+                try {
+                    aeg = artikkel.select(".article__publish-date").first().text();
+                } catch (Exception e) {
+                    aeg = "-";
+                }
+                //System.out.println(aeg);
+
+                artikliSisu = "";
+                for (Element tekst : artikkel.select(".article-body__item").select("p")) {
+                    artikliSisu = artikliSisu + tekst.text() + "\n";
+                }
+                //System.out.println(artikliSisu);
+                //System.out.println();
+
+                tagastus.add(new Fetcher(pealkiri, link, aeg, artikliSisu, "Postimees"));
+            } catch (Exception e) {
+                continue;
             }
-            System.out.println(artikliSisu);
-            System.out.println();
         }
+        //TimeUnit.SECONDS.sleep(1);
+        return tagastus;
     }
 }
--- src/TestFetcher.java
+++ src/TestFetcher.java
@@ -3,8 +3,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.ArrayList;
+
 public class TestFetcher {
     public static void main(String[] args) throws Exception {
-        Fetcher.getPostimees();
+        ArrayList<Fetcher> asd = Fetcher.getPostimees();
+        System.out.println(asd.size());
+        System.out.println(asd.get(3).getPealkiri() + " - " + asd.get(3).getAeg());
     }
 }
