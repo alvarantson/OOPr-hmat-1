d20638489ecfb2ad6e52c7ffa9c3cb212140fa1c
Alvar Antson laptop <alvar.antson@real.edu.ee>
2019-03-14 17:12:45 +0200

    asshole

--- src/Fetcher.java
+++ src/Fetcher.java
@@ -14,6 +14,7 @@ public class Fetcher {
     private String aeg;
     private String artikkel;
     private String portaal;
+    private int counter;
 
     public Fetcher(String pealkiri, String link, String aeg, String artikkel, String portaal) {
         this.pealkiri = pealkiri;
